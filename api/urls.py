8cb25456d7ceae186fed0ad27935d9e62c3741f6
osmankurbanov1 <letter.to.osman@gmail.com>
2022-05-09 01:42:04 +0600

    Added new endpoints (posts, comments)

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
     path('profiles/<int:pk>/', views.UsersProfileView.as_view()),
+    path('user-posts/<int:pk>/', views.PostListView.as_view()),
+    path('posts/', views.PostView.as_view({'post': 'create'})),
+    path('posts/<int:pk>/', views.PostView.as_view({
+        'get': 'retrieve', 'put': 'update', 'delete': 'destroy'
+    })),
+    path('comments/', views.CommentListCreateView.as_view()),
+    path('comments/<int:pk>/', views.CommentsView.as_view()),
 ]
