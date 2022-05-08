c3c7b6c9906aac03d4064bf967d9a904352fc004
osmankurbanov1 <letter.to.osman@gmail.com>
2022-05-09 01:39:06 +0600

    Refactored MyUser model attributes

--- profiles/models.py
+++ profiles/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
 
 class MyUser(AbstractUser):
-    """custom user model"""
+    """custom User model"""
     MALE = 'M'
     FEMALE = 'F'
     GENDER_CHOICES = [
         (MALE, 'Male'),
         (FEMALE, 'Female'),
     ]
-    first_login = models.DateTimeField(null=True)
-    phone = models.CharField(max_length=14)
-    user_photo = models.ImageField(upload_to='media/MEDIA_USER_IMAGE_DIR/avatar/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MALE)
+    user_photo = models.ImageField(upload_to='media/MEDIA_USER_IMAGE_DIR/avatar/', blank=True, null=True)
+    user_gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MALE)
+    first_login = models.DateTimeField(null=True)
