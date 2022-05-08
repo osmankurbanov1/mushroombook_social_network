a605ccfa65e9a7c8e80166f61b85150cc26a36b4
osmankurbanov1 <letter.to.osman@gmail.com>
2022-05-09 01:40:25 +0600

    Added new serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,48 @@
 from rest_framework import serializers
+
 from profiles.models import MyUser
+from wall.models import Post, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
     """
     Serializing all Users
     """
-    #  avatar = serializers.ImageField(read_only=True)
 
     class Meta:
         model = MyUser
-        exclude = (
-            "email",
-            "phone",
-            "password",
-            "is_active",
-            "is_staff",
-            "is_superuser",
-            "groups",
-            "user_permissions"
-        )
+        fields = (
+            "id", "username", "first_name", "last_name", "user_gender", "user_photo",
+            "bio", "date_joined", "last_login", "first_login",
+            )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializing comments
+    """
+
+    comment_author = serializers.ReadOnlyField(source='user.username')
+    is_deleted = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Comment
+        fields = ("id", "comment_author", "post", "text", "parent", "is_deleted")
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializing all Posts
+    """
+
+    post_author = serializers.ReadOnlyField(source='user.username')
+    post_is_published = serializers.ReadOnlyField(default=True)
+    comments = CommentSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id", "post_created_date", "post_is_published",
+            "post_author", "post_description", "list_of_mushrooms",
+            "comments",
+            )
