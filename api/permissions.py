1a40fea41934ec2d91ef979d434d11635c4f381c
osmankurbanov1 <letter.to.osman@gmail.com>
2022-05-09 01:39:49 +0600

    Added new permissions

--- api/permissions.py
+++ api/permissions.py
@@ -2,7 +2,30 @@ from rest_framework import permissions
 
 
 class IsProfileOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Is profile owner?
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+
+class IsPostAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Is post author?
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.post_author == request.user
+
+
+class IsCommentAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Is comment author?
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.comment_author == request.user
