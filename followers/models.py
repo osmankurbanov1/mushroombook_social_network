456aa1bae39bbadc769e19a2bfddcd13e9694611
osmankurbanov1 <letter.to.osman@gmail.com>
2022-05-12 00:19:30 +0600

    initial commit of Followers app

---
+++ followers/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+
+class Follower(models.Model):
+    """
+    follower_user follows user
+    """
+    # Who follows - follower_user
+    follower_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following',
+        verbose_name='Who follows'
+                                      )
+    # To whom - user
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers",
+        verbose_name='To whom'
+    )
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.follower_user} follows {self.user}'
