73c65b11d035f0fa76eb860caceb8d0ee32ca6a7
osmankurbanov1 <letter.to.osman@gmail.com>
2022-05-12 23:52:14 +0600

    Added unique_together

--- followers/models.py
+++ followers/models.py
@@ -4,20 +4,23 @@ from django.conf import settings
 
 class Follower(models.Model):
     """
-    follower_user follows following_user
+    Follower/Following user model
     """
-    # Who follows - follower_user
+    objects = models.Manager()
+
+    # Who follows
     follower_user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following',
         verbose_name='Who follows'
                                       )
-    # To whom - following_user
+    # To whom
     following_user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers",
         verbose_name='To whom'
     )
 
-    objects = models.Manager()
-
     def __str__(self):
         return f'{self.follower_user} follows {self.following_user}'
+
+    class Meta:
+        unique_together = (('follower_user', 'following_user'), )
